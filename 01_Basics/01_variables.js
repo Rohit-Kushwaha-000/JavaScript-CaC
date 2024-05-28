73f15451ef79dcc9629895160911b9427835afb6
Rohit-Kushwaha-000 <75386533+Rohit-Kushwaha-000@users.noreply.github.com>
2024-05-28 03:25:36 +0000

    Change Config Commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553;
+let accountEmail = "rohit@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2 //not allowed to change const value
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+accountEmail = "rk@google.com"
+accountPassword = "335322"
+accountCity = "Delhi";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
