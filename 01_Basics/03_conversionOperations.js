4a46440b1a344f97f1e0995a027fc9cdbb2f2632
Rohit-Kushwaha-000 <75386533+Rohit-Kushwaha-000@users.noreply.github.com>
2024-05-29 04:01:36 +0000

    Conversion Operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+// let score = "Rohit";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueIsNumber = Number(score);
+// console.log(typeof valueIsNumber);
+// console.log(valueIsNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false => 0;
+
+let isLoggedIn = "55";
+let booleanIsLoggedIN = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIN)
+
+// 1 => true; 0 => false;
+// " " => false
+// "Rohit" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
